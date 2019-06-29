2325a6e00899fe2148bb716e2d781a3c69ac6e12
derekSaga <derek.coelho@outlook.com.br>
2019-06-29 12:47:47 -0300

    primeira

---
+++ carro.py
@@ -0,0 +1,52 @@
+class Carro():
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado,
+                 velocidade):
+        """
+        ESSA CLASSE É UTILIZADA PARA INSTANCIAR NOVOS CARROS
+        :type acelerar: int
+        :type qtd_combustivel: float
+        :type cor: object
+        :type qtd_portas: object
+        :type tipo_combustivel: object
+        :type potencia: float
+        :type is_ligado: bool
+        """
+        self.cor = cor
+        self.qtd_portas = qtd_portas
+        self.tipo_combustivel = tipo_combustivel
+        self.potencia = potencia
+        self.qtd_combustivel = qtd_combustivel
+        self.is_ligado = is_ligado
+        self.velocidade = velocidade
+
+    def abastecer(self, qtd_combustivel=0):
+        """
+        :type qtd_combustivel: float
+        RECEBE COMO PARAMETRO A QUANTIDADE DE COMBUSTÍVEL E INCREMENTA O ATRIBUTO qtd_combustivel DO OBJETO CARRO
+        """
+        self.qtd_combustivel = + qtd_combustivel
+
+    def ligado(self):
+        if self.is_ligado:
+            print("O carro já está ligado !")
+        else:
+            self.is_ligado = True
+            print('O carro foi ligado !')
+
+    def desligado(self):
+        if self.is_ligado == False:
+            print('O carro já está desligado !')
+        else:
+            self.is_ligado = False
+            print('O carro foi desligado !')
+
+    def acelerar(self, velocidade=10):
+        """
+
+        :param velocidade: int
+        :return: None
+        """
+        if self.is_ligado:
+            self.velocidade += velocidade
+        else:
+            print('O carro precisa estar ligado para ser acelerado !')
---
+++ main.py
@@ -0,0 +1,6 @@
+import carro
+
+uno_preto = carro.Carro('azul', 4, 'Diesel', 3.0, 0, False, velocidade=0)
+
+help(uno_preto.abastecer)
+help(carro.Carro)
