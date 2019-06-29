ad120c43042157306636743ae9f39b1fe1d00ed3
derekSaga <derek.coelho@outlook.com.br>
2019-06-29 12:59:44 -0300

    terceiro

--- carro.py
+++ carro.py
@@ -1,6 +1,5 @@
 class Carro():
-    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia, qtd_combustivel, is_ligado,
-                 velocidade):
+    def __init__(self, cor, qtd_portas, tipo_combustivel, potencia):
         """
         ESSA CLASSE É UTILIZADA PARA INSTANCIAR NOVOS CARROS
         :type acelerar: int
@@ -8,16 +7,14 @@ class Carro():
         :type cor: object
         :type qtd_portas: object
         :type tipo_combustivel: object
-        :type potencia: float
-        :type is_ligado: bool
         """
         self.cor = cor
         self.qtd_portas = qtd_portas
         self.tipo_combustivel = tipo_combustivel
         self.potencia = potencia
-        self.qtd_combustivel = qtd_combustivel
-        self.is_ligado = is_ligado
-        self.velocidade = velocidade
+        self.qtd_combustivel = 0
+        self.is_ligado = False
+        self.velocidade = 0
 
     def abastecer(self, qtd_combustivel=0):
         """
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import carro
 
-uno_preto = carro.Carro('azul', 4, 'Diesel', 3.0, 0, False, velocidade=0)
+uno_preto = carro.Carro('azul', 4, 'Diesel', 3.0)
